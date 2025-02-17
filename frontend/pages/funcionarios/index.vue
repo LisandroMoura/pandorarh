66002fb490be1afb4f419a06f4277ddebfdabbd6
lisandromoura <lisandro.cabrom@gmail.com>
2025-02-17 16:32:17 -0300

    Renderizar lista de funcionários dinamicamente no componente ListItem

--- frontend/pages/funcionarios/index.vue
+++ frontend/pages/funcionarios/index.vue
@@ -15,11 +15,8 @@
 
       <!-- Itens -->
       <div class="grid grid-cols-1 gap-4">
-        <ListItem />
-        <ListItem />
-        <ListItem />
-        <ListItem />
-        <ListItem />
+        <ListItem v-for="funcionario in funcionarios" :key="funcionario.id" :id="funcionario.id" :nome="funcionario.nome"
+          :data_nascimento="funcionario.data_nascimento" />
       </div>
     </div>
   </div>
@@ -27,6 +24,12 @@
 
 <script setup>
 // TODO: requisitar os dados do backend
+const funcionarios = ref([
+  { id: 1, nome: "João da Silva", data_nascimento: "01/01/1990" },
+  { id: 2, nome: "Maria Oliveira", data_nascimento: "05/07/1985" },
+  { id: 3, nome: "Carlos Souza", data_nascimento: "20/11/1992" }
+]);
+
 </script>
 
 <style></style>
